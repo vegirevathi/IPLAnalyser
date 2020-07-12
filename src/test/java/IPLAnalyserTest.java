8ec847cd7d7be8671ce5cc684f90743a1424f810
vegirevathi <vegirevathi@gmail.com>
2020-07-12 15:05:17 +0530

    ADDED: Given IPl most runs csv file should return top batting average player

--- src/test/java/IPLAnalyserTest.java
+++ src/test/java/IPLAnalyserTest.java
@@ -1,4 +1,7 @@
+import IPLAnalyser.exception.IPLAnalyserException;
+import IPLAnalyser.model.MostRunCSV;
 import IPLAnalyser.service.IPLAnalyser;
+import com.google.gson.Gson;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -20,5 +23,18 @@ public class IPLAnalyserTest {
         IPLAnalyser iplAnalyser = new IPLAnalyser();
         int numOfRecords = iplAnalyser.loadIPLMostWktsData(IPL_MOST_WKTS_CSV_FILE_PATH);
         Assert.assertEquals(100,numOfRecords);
+    }
+
+    @Test
+    public void givenIPLMostRunsCSVFile_WhenSortedOnAvg_ShouldReturnCorrectDesiredSortedData() {
+        try {
+            IPLAnalyser iplAnalyser = new IPLAnalyser();
+            iplAnalyser.loadIPLMostRunsData(IPL_MOST_RUNS_CSV_FILE_PATH);
+            String iplpLayersRecords = iplAnalyser.getAvgWiseSortedIPLPLayersRecords(IPL_MOST_RUNS_CSV_FILE_PATH);
+            MostRunCSV[] mostRunsCSV = new Gson().fromJson(iplpLayersRecords, MostRunCSV[].class);
+            Assert.assertEquals("MS Dhoni", mostRunsCSV[mostRunsCSV.length - 1].player);
+        } catch (IPLAnalyserException e) {
+            e.printStackTrace();
+        }
     }
 }
