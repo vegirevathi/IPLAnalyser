4738c93e022815a46ad644423d32c6ae6d5dd7ff
vegirevathi <vegirevathi@gmail.com>
2020-07-12 14:48:24 +0530

    ADDED: Given most wkts csv data should return correct records

--- src/test/java/IPLAnalyserTest.java
+++ src/test/java/IPLAnalyserTest.java
@@ -6,11 +6,19 @@ import java.io.IOException;
 
 public class IPLAnalyserTest {
     private static final String IPL_MOST_RUNS_CSV_FILE_PATH = "./src/test/resources/IPL2019FactsheetMostRuns.csv";
+    private static final String IPL_MOST_WKTS_CSV_FILE_PATH = "./src/test/resources/IPL2019FactsheetMostWkts.csv";
 
     @Test
     public void givenIPLMostRunsCSVFile_ShouldReturnCorrectRecords() {
         IPLAnalyser iplAnalyser = new IPLAnalyser();
         int numOfRecords = iplAnalyser.loadIPLMostRunsData(IPL_MOST_RUNS_CSV_FILE_PATH);
+        Assert.assertEquals(100,numOfRecords);
+    }
+
+    @Test
+    public void givenIPLMostWktsCSVFile_ShouldReturnCorrectRecords() {
+        IPLAnalyser iplAnalyser = new IPLAnalyser();
+        int numOfRecords = iplAnalyser.loadIPLMostWktsData(IPL_MOST_WKTS_CSV_FILE_PATH);
         Assert.assertEquals(100,numOfRecords);
     }
 }
