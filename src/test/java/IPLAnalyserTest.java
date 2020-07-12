f6555c008885d5941e5127d301b9b4838384ea63
vegirevathi <vegirevathi@gmail.com>
2020-07-12 14:38:37 +0530

    ADDED: Given IPL csv file should return total records

--- src/test/java/IPLAnalyserTest.java
+++ src/test/java/IPLAnalyserTest.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import IPLAnalyser.service.IPLAnalyser;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
 
 public class IPLAnalyserTest {
+    private static final String IPL_MOST_RUNS_CSV_FILE_PATH = "./src/test/resources/IPL2019FactsheetMostRuns.csv";
+
+    @Test
+    public void givenIPLMostRunsCSVFile_ShouldReturnCorrectRecords() {
+        IPLAnalyser iplAnalyser = new IPLAnalyser();
+        int numOfRecords = iplAnalyser.loadIPLMostRunsData(IPL_MOST_RUNS_CSV_FILE_PATH);
+        Assert.assertEquals(100,numOfRecords);
+    }
 }
